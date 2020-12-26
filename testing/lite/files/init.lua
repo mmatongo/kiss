8c42ae71eb95c9e198276d6c91e709213a61acbb
Daniel M. Matongo <me@danielm.pw>
2020-12-26 16:53:36 +0000

    lite: add init file

---
+++ testing/lite/files/init.lua
@@ -0,0 +1,15 @@
+-- put user settings here
+-- this module will be loaded after everything else when the application starts
+
+local keymap = require "core.keymap"
+local config = require "core.config"
+local style = require "core.style"
+
+require "user.colors.duotone"
+
+-- light theme:
+-- require "user.colors.summer"
+
+-- key binding:
+-- keymap.add { ["ctrl+escape"] = "core:quit" }
+
